a37add2cc4ac2f5e57102a515f71bcd59efe4e8a
DimaShumanskiy <dimaihfn@gmail.com>
2021-12-04 18:41:05 +0300

    jest and test stories

--- src/stories/Task.stories.tsx
+++ src/stories/Task.stories.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {ComponentStory, ComponentMeta} from '@storybook/react';
 import {AddItemForm} from "../AddItemForm";
 import {action} from "@storybook/addon-actions";
@@ -10,24 +10,35 @@ export default {
     title: 'TodoList/Task',
     component: Task,
     args:{
-        removeTask: action('removeTask'),
-        changeTaskStatus: action('changeTaskStatus'),
-        changeTaskTitle: action('changeTaskTitle'),
+        // removeTask: action('removeTask'),
+        // changeTaskStatus: action('changeTaskStatus'),
+        // changeTaskTitle: action('changeTaskTitle'),
 }
 } as ComponentMeta<typeof Task>;
 
-const Template: ComponentStory<typeof Task> = (args) => <Task {...args} />;
+const Template: ComponentStory<typeof Task> = (args) => {
+    const [task , setTask] = useState({id: '1', isDone: true , title: 'react'})
+    const changeTaskStatus = () => setTask({id: '1', isDone: !task.isDone , title: 'react'})
+    return <Task
+        task = {task}
+        removeTask = {action('changeTaskTitle')}
+        changeTaskStatus = {changeTaskStatus}
+        changeTaskTitle={action('changeTaskTitle')}
+        // {...args}
+    />
+};
 
 export const TaskIsDoneStory = Template.bind({});
-TaskIsDoneStory.args = {
-    task: {id: '1', isDone: true , title: 'react'},
-}
 
-export const TaskIsNotStory = Template.bind({});
-
-TaskIsNotStory.args = {
-    task: {id: '2', isDone: false , title: 'js'},
-}
+// TaskIsDoneStory.args = {
+//     task: {id: '1', isDone: true , title: 'react'},
+// }
+//
+// export const TaskIsNotStory = Template.bind({});
+//
+// TaskIsNotStory.args = {
+//     task: {id: '2', isDone: false , title: 'js'},
+// }
 
 
 
