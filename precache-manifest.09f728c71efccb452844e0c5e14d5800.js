52e9319bd04f3aba745c56df7418e90eb71bf29b
DimaShumanskiy <dimaihfn@gmail.com>
2022-01-24 21:03:16 +0300

    Updates

---
+++ precache-manifest.09f728c71efccb452844e0c5e14d5800.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "46ef1e4b2758fc1f4bace231bf9389ba",
+    "url": "/project_TodoList_ts/index.html"
+  },
+  {
+    "revision": "b8ac936e4b405a00d9dc",
+    "url": "/project_TodoList_ts/static/css/main.155097fa.chunk.css"
+  },
+  {
+    "revision": "cc328fc375f7f79a9e41",
+    "url": "/project_TodoList_ts/static/js/2.fc85ff32.chunk.js"
+  },
+  {
+    "revision": "e2896a34bc173bbd7b6d83453e91d5e1",
+    "url": "/project_TodoList_ts/static/js/2.fc85ff32.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b8ac936e4b405a00d9dc",
+    "url": "/project_TodoList_ts/static/js/main.4b26fb93.chunk.js"
+  },
+  {
+    "revision": "dd1adc906b31017b6cc9",
+    "url": "/project_TodoList_ts/static/js/runtime-main.75384ab7.js"
+  }
+]);
